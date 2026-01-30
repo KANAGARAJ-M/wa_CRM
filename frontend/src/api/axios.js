e835524f76730ba855fe5bf8203768a8201e2ac2
KANAGARAJ-M <kanagarajm638@gmail.com>
2026-01-30 13:36:01 +0530

    fix switching company

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://srv1304549.hstgr.cloud/api', // Production
-    baseURL: 'http://localhost:3001/api', // Local Development
+    baseURL: 'https://srv1304549.hstgr.cloud/api', // Production
+    // baseURL: 'http://localhost:3001/api', // Local Development
 });
 
 api.interceptors.request.use((config) => {
