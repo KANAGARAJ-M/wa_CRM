c6c759a91fbd5733a482e28ce257fda0734eb091
KANAGARAJ-M <kanagarajm638@gmail.com>
2026-02-06 16:29:16 +0530

    update the cros - srv push

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://wa-crm-app.netlify.app'],
+    origin: ['http://localhost:5173', 'https://wa-crm-app.netlify.app', 'http://localhost:5174'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-company-id']
